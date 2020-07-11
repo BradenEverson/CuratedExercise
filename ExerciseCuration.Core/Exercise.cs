70740a8b87fa9d775058262bf36937264dc91bdb
Braden Everson <bradeneverson@gmail.com>
2020-07-11 12:48:53 -0500

    Created core exercise class

---
+++ ExerciseCuration.Core/Exercise.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciseCuration.Core
+{
+    public class Exercise
+    {
+        public string instructions { get; }
+        public workoutTypes workoutType { get; }
+        public int userRating { get; set; }
+    }
+    public enum workoutTypes
+    {
+        ActiveRecovery,
+        Aerobic,
+        Anaerobic,
+        Circuit,
+        HIIT,
+        Isometric,
+        StrengthTraining
+    }
+}
