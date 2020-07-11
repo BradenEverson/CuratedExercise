d8c0ba2803a3673b759beb7bd180d9c316243384
Braden Everson <bradeneverson@gmail.com>
2020-07-11 13:06:23 -0500

    Created interface for database of exercises, will add more functionality later

---
+++ ExerciseCuration.Data/IExerciseData.cs
@@ -0,0 +1,15 @@
+﻿using ExerciseCuration.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciseCuration.Data
+{
+    public interface IExerciseData
+    {
+        public Exercise add(Exercise exercise);
+        public Exercise delete(int id);
+        public Exercise update(Exercise updatedExercise);
+        public Exercise getById(int id);
+    }
+}
