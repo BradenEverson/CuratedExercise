d57cb0077550f7742a2b95bcd008ced869ce7ef4
Braden Everson <bradeneverson@gmail.com>
2020-07-11 13:39:56 -0500

    Configured signalr services and exercise hub

--- CuratedExercise/Startup.cs
+++ CuratedExercise/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ExerciseCuration.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,8 @@ namespace CuratedExercise
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSignalR();
+            services.AddSingleton<IExerciseData, InMemoryExerciseDb>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +53,7 @@ namespace CuratedExercise
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapHub<ExerciseHub>("ExcerciseHub");
             });
         }
     }
